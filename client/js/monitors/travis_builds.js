4919a4ca26a7b3c0ae32c6f253974eed6e1c4667
Matthew Taylor <matt@numenta.org>
2013-12-14 00:07:29 -0800

    Split build history into master and prs.

--- client/js/monitors/travis_builds.js
+++ client/js/monitors/travis_builds.js
@@ -9,11 +9,24 @@ $(function() {
             }
           ;
         server.get('history', slug, function(responseData) {
-            var history = $.extend({}, responseData, slug);
-            _.each(responseData.builds, function(build) {
+            var history = $.extend({pr: false}, slug, responseData);
+            _.each(history.builds, function(build) {
                 build.status = WB.utils.travisStateToStatus(build.state);
                 build.started_at = WB.utils.formatDate(build.started_at);
             });
+            if (config.pull_requests) {
+                history.pr = true;
+                history.type = 'pr';
+                history.builds = _.filter(history.builds, function(build) {
+                    return build.pull_request;
+                });
+            } else {
+                history.type = 'master'
+                history.builds = _.filter(history.builds, function(build) {
+                    return ! build.pull_request;
+                });
+              }
+            console.log(history);
             template(history);
         });
     };
