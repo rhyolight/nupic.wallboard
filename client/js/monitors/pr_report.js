fccdeb423be39795eda92fc71c8c5807e60653f7
Matthew Taylor <matt@numenta.org>
2014-01-17 10:59:39 -0800

    Added overall status report.
    Fixes #16

--- client/js/monitors/pr_report.js
+++ client/js/monitors/pr_report.js
@@ -1,6 +1,7 @@
 $(function() {
 
     var OVERDUE_GAP = 7 * 24 * 60 * 60 * 1000
+      , TIMEOUT = 10000
       , SLUG = 'numenta/nupic'
       , prUrl = 'http://issues.numenta.org:8081/prStatus?repo=' + SLUG + '&callback=?';
 
@@ -9,25 +10,36 @@ $(function() {
           , overdueCount = 0
           ;
 
-        $.getJSON(prUrl, function(prs) {
-            prs.sort(function(a, b) {
-                return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
-            }).forEach(function(pr) {
-                if ((now - new Date(pr.created_at).getTime()) > OVERDUE_GAP) {
-                    pr.overdue = true;
-                    overdueCount++;
-                }
-                // pr.created_at = pr.created_at.split('T').shift();
-                pr.created_at = WB.utils.timeAgo(pr.created_at)
-                pr.latest_status = pr.statuses[0];
-            });
-            template({
-                slug: SLUG
-              , overdue: overdueCount
-              , open: prs.length
-              , prs: prs
-            });
+        $.ajax(prUrl, {
+            dataType: 'jsonp'
+          , timeout: TIMEOUT
+          , error: function(err) {
+                console.log(arguments);
+                template({
+                    status: 'timeout'
+                });
+            }
+          , success: function(prs) {
+                prs.sort(function(a, b) {
+                    return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
+                }).forEach(function(pr) {
+                    if ((now - new Date(pr.created_at).getTime()) > OVERDUE_GAP) {
+                        pr.overdue = true;
+                        overdueCount++;
+                    }
+                    // pr.created_at = pr.created_at.split('T').shift();
+                    pr.created_at = WB.utils.timeAgo(pr.created_at)
+                    pr.latest_status = pr.statuses[0];
+                });
+                template({
+                    slug: SLUG
+                  , overdue: overdueCount
+                  , open: prs.length
+                  , prs: prs
+                });
+            }
         });
+
     };
 
     window.WB.pr_report = initialize;
