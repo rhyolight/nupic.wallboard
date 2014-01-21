629aa8d069b96dd3767d99b6c8b9dbe235b47d8b
Jeff Fohl <jeff@fohl.com>
2014-01-21 10:47:41 -0800

    resolves numenta/nupic.wallboard#13 Added github labels to PR report.

--- client/js/monitors/pr_report.js
+++ client/js/monitors/pr_report.js
@@ -1,13 +1,21 @@
+
 $(function() {
 
     var OVERDUE_GAP = 7 * 24 * 60 * 60 * 1000
       , prUrl = "http://issues.numenta.org:8081/prStatus?repo=numenta/nupic&callback=?";
 
+
     function initialize(id, config, server, template) {
+        
+        // we are going to extend jQuery with this convenience method, which will help us know when all promises have been resolved.
+        $.whenall = function(arr) {
+            return $.when.apply($, arr);
+        };
+    
         var now = new Date().getTime()
           , overdueCount = 0
+          , promises = [] // create an empty array to hold our promises
           ;
-
         $.getJSON(prUrl, function(prs) {
             prs.sort(function(a, b) {
                 return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
@@ -17,13 +25,29 @@ $(function() {
                     overdueCount++;
                 }
                 // pr.created_at = pr.created_at.split('T').shift();
-                pr.created_at = WB.utils.timeAgo(pr.created_at)
+                pr.created_at = WB.utils.timeAgo(pr.created_at);
                 pr.latest_status = pr.statuses[0];
+                var deferred = new $.Deferred();
+                var slug = {
+                    owner: "numenta"
+                    , repo: "nupic"
+                    , number: pr.number
+                };
+                server.get('labels', slug, function(responseData) {
+                    deferred.resolve(responseData.labels);
+                });
+                promises.push(deferred);
             });
-            template({
-                overdue: overdueCount
-              , open: prs.length
-              , prs: prs
+            // when all promises are resolved, we attach the results to the pull results array, then send the data to the template.
+            $.whenall(promises).done(function() {
+                for (var i = 0; i < arguments.length; i++) {
+                    prs[i].labels = arguments[i];
+                }
+                template({
+                    overdue: overdueCount
+                  , open: prs.length
+                  , prs: prs
+                });
             });
         });
     };
