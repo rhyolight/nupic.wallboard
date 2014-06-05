08c8e76687373e5d93e173030ce6d546b146a870
Matthew Taylor <rhyolight@gmail.com>
2014-06-05 15:12:39 -0700

    added view for running travis builds.

---
+++ client/js/builds/builds.js
@@ -0,0 +1,49 @@
+$(function() {
+
+    var buildsDivId = 'build-container'
+
+    function loadTemplate(src, id, callback) {
+        $.ajax({
+            url: src,
+            success: function(resp) {
+                var $script = $('<script type="text/template" id="' + id + '_tmpl">' + resp + '</script>');
+                $('body').append($script);
+                callback(null, id + '_tmpl');
+            },
+            failure: callback
+        });
+    }
+
+    function buildTravisUrl(repoSlug, buildId) {
+        return 'https://travis-ci.org/' + repoSlug + '/builds/' + buildId;
+    }
+
+    function buildPullRequestUrl(repoSlug, prId) {
+        return 'https://github.com/' + repoSlug + '/pull/' + prId;
+    }
+
+    function renderBuilds(templateId, builds) {
+        console.log(builds);
+        _.each(builds, function(repoBuilds, repoName) {
+            _.each(repoBuilds, function(build) {
+                build.started_ago = moment(build.started_at).from(new Date());
+                build.html_url = buildTravisUrl('numenta/' + repoName, build.id);
+                if (build.pull_request) {
+                    build.pr_url = buildPullRequestUrl('numenta/' + repoName, build.pull_request_number);
+                }
+            });
+        });
+        template = Handlebars.compile($('#' + templateId).html())
+        $('#' + buildsDivId).html(template({builds: builds}));
+    }
+
+    loadTemplate('/js/builds/builds.html', 'builds', function(err, templId) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(templId + ' template loaded');
+        $.getJSON('/_builds/', function(builds) {
+            renderBuilds(templId, builds);
+        });
+    });
+});
