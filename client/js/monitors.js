3d02a1a5f42dbd6298349d892ef480ace0c82e5e
Matthew Taylor <rhyolight@gmail.com>
2014-07-15 15:32:51 -0700

    Breaking out monitor templates into own dir.

--- client/js/monitors.js
+++ client/js/monitors.js
@@ -169,8 +169,10 @@ $(function() {
         _.each(monitors, function(monitorConfig, monitorId) {
             var scriptPath = monitorConfig.js
               , loadingHtml = '<img src="/images/ajax-loader.gif" alt="loading"/>'
-              , namespace = scriptPath.split('/').pop().split('.').shift()
-              , templatePath = scriptPath.split('.js')[0] + '.html'
+              , scriptName = scriptPath.split('/').pop()
+              , scriptDirectory = scriptPath.substr(0, scriptPath.length - scriptName.length)
+              , namespace = scriptName.split('.').shift()
+              , templatePath = scriptDirectory + '/tmpl/' + namespace + '.html'
               ;
             async.parallel([
                 function(callback) {
