90ca2ca1700405e7269cf653b47b8a89c485cd6e
Matthew Taylor <rhyolight@gmail.com>
2014-07-16 16:04:09 -0700

    More status styling updates.

--- client/js/monitors/latest_issues.js
+++ client/js/monitors/latest_issues.js
@@ -4,6 +4,7 @@ $(function() {
         server.get('issues', null, function(response) {
             response.title = 'Latest ' + response.issues.length + ' Updated Issues';
             _.each(response.issues, function(issue) {
+                console.log(issue.assignee);
                 issue.updated = WB.utils.timeAgo(issue.updated_at);
             });
             template(response);
