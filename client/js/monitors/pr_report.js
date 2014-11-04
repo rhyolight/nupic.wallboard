4cf6cd946f6f75caa8891a81300a338cc788176b
Matthew Taylor <rhyolight@gmail.com>
2014-11-03 17:30:22 -0800

    Initial redo of PR status page.

--- client/js/monitors/pr_report.js
+++ client/js/monitors/pr_report.js
@@ -2,69 +2,35 @@
 $(function() {
 
     var OVERDUE_GAP = 7 * 24 * 60 * 60 * 1000
-      , TIMEOUT = 10000
-      , SLUG = 'numenta/nupic'
-      , prUrl = 'http://issues.numenta.org:8081/prStatus?repo=' + SLUG + '&callback=?';
-
+      , validateUrl = "http://issues.numenta.org:8081/validate";
 
     function initialize(id, config, server, template) {
-        
-        // we are going to extend jQuery with this convenience method, which will help us know when all promises have been resolved.
-        $.whenall = function(arr) {
-            return $.when.apply($, arr);
-        };
-    
-        var now = new Date().getTime()
-          , overdueCount = 0
-          , promises = [] // create an empty array to hold our promises
-          ;
+        server.get('pulls', null, function(prs) {
+            var now = new Date().getTime()
+              , overdueCount = 0
+              ;
 
-        $.ajax(prUrl, {
-            dataType: 'jsonp'
-          , timeout: TIMEOUT
-          , error: function(err) {
-                console.log(arguments);
-                template({
-                    status: 'timeout'
-                });
-            }
-          , success: function(prs) {
-                prs.sort(function(a, b) {
-                    return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
-                }).forEach(function(pr) {
-                    if ((now - new Date(pr.created_at).getTime()) > OVERDUE_GAP) {
-                        pr.overdue = true;
-                        overdueCount++;
-                    }
-                    // pr.created_at = pr.created_at.split('T').shift();
-                    pr.created_at = WB.utils.timeAgo(pr.created_at)
-                    pr.latest_status = pr.statuses[0];
-                    var deferred = new $.Deferred();
-                    var slug = {
-                        owner: "numenta"
-                        , repo: "nupic"
-                        , number: pr.number
-                    };
-                    server.get('labels', slug, function(responseData) {
-                        deferred.resolve(responseData.labels);
-                    });
-                    promises.push(deferred);
-                });
-                // when all promises are resolved, we attach the results to the pull results array, then send the data to the template.
-                $.whenall(promises).done(function() {
-                    for (var i = 0; i < arguments.length; i++) {
-                        prs[i].labels = arguments[i];
-                    }
-                    template({
-                        slug: SLUG
-                      , overdue: overdueCount
-                      , open: prs.length
-                      , prs: prs
-                    });
-                });
-            }
+            prs.sort(function(a, b) {
+                return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
+            }).forEach(function(pr) {
+                if ((now - new Date(pr.created_at).getTime()) > OVERDUE_GAP) {
+                    pr.overdue = true;
+                    overdueCount++;
+                }
+                pr.created_at = WB.utils.timeAgo(pr.created_at);
+                if (pr.builds && pr.builds.length) {
+                    pr.latest_status = pr.builds[0];
+                }
+            });
+            template({
+                overdue: overdueCount
+              , validateUrl: validateUrl
+              , open: prs.length
+              , prs: prs
+            });
         });
-    };
+
+    }
 
     window.WB.pr_report = initialize;
 
